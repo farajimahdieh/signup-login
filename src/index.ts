687026aa30831c7f315adc3c98c93089115740c8
mahdieh <mahdiehfaraji194@gmail.com>
2025-08-11 12:06:03 +0330

    setingup the env

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import express from "express"
 const app = express()
 
 
+
+
+app.post("/register", (req,res) =>{
+
+
+})
 app.listen(3000, () => {
     console.log("it is running, hooooooora!")
 })
